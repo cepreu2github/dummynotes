1e0c91fc3aadef5cd7c96bdf935146789703a4f2
Cepreu <cepreu.mail@gmail.com>
2015-10-20 07:44:37 +0300

    material action bar

--- DummyNotes/app/src/main/java/ru/myitschool/dummynotes/MainActivity.java
+++ DummyNotes/app/src/main/java/ru/myitschool/dummynotes/MainActivity.java
@@ -1,8 +1,14 @@
 package ru.myitschool.dummynotes;
 
+import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,17 +17,19 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    // use this icons: http://icons4android.com
 
     private Context mContext = this;
     private Toolbar mToolbar;
+    private SharedPreferences mPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(mToolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         final ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(new NotesAdapter(this));
@@ -31,12 +39,25 @@ public class MainActivity extends ActionBarActivity {
                 Toast.makeText(mContext, "Click at " + position, Toast.LENGTH_SHORT).show();
             }
         });
+        // Search work
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doSearch(query);
+        }
+    }
+
+    private void doSearch(String query){
+        Toast.makeText(mContext, "Searching: " + query, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
@@ -48,10 +69,18 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_sort){
+            Toast.makeText(mContext, "Sort", Toast.LENGTH_SHORT).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    protected void onSaveInstanceState (Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        //FIXME: save current list position, opened note id, sorting mode
+    }
+
 }
